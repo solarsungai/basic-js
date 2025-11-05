704f3ff415dad4a00ebe4aa5a6a0917b1ae79d54
Margarita Dobrenco <solarsungai@gmail.com>
2025-11-05 20:46:17 +0200

    feat: mac-adress and matrix-elements-sum done

--- src/mac-address.js
+++ src/mac-address.js
@@ -14,9 +14,13 @@ const { NotImplementedError } = require('../lib');
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function isMAC48Address(n) {
+  const parts = n.split(/[:-]/);
+  if (parts.length !== 6) return false;
+
+  return parts.every(
+    part => /^[0-9A-Fa-f]{2}$/.test(part)
+  );
 }
 
 module.exports = {
