f61702349ab02b13a6f6d95da599a681688132ef
Margarita Dobrenco <solarsungai@gmail.com>
2025-11-06 12:35:39 +0200

    feat: sum-digits and transform-array done

--- src/sum-digits.js
+++ src/sum-digits.js
@@ -12,9 +12,17 @@ const { NotImplementedError } = require('../lib');
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function getSumOfDigits(n) {
+  if (n < 10) {
+    return n;
+  } else {
+    const digitsArr = String(n).split('').map(Number);
+    const sum = digitsArr.reduce((acc, digit) => {
+      acc += digit;
+      return acc;
+    }, 0);
+    return getSumOfDigits(sum);
+  }
 }
 
 module.exports = {
--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,32 @@ const { NotImplementedError } = require('../lib');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  *
  */
-function transform(/* arr */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  };
+  let skipNext = false;
+
+  let resultArr = arr.reduce((acc, el, i) => {
+    if (skipNext) {
+      skipNext = false;
+      return acc;
+    } 
+    if (el === '--discard-next') {
+      skipNext = true;
+    } else if (el === '--discard-prev') {
+      if (acc.length > 0 && arr[i - 2] !== '--discard-next') acc.pop();
+    } else if (el === '--double-next') {
+      if (i + 1 < arr.length) acc.push(arr[i + 1]);
+    } else if (el === '--double-prev') {
+      if (i - 1 >= 0 && arr[i - 2] !== '--discard-next') acc.push(arr[i - 1]);
+    } else {
+      acc.push(el);
+    };
+    return acc;
+  }, []);
+
+  return resultArr;
 }
 
 module.exports = {
